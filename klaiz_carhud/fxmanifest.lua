fff856961a9f2b23c174d80cd0d52fd0d76efa1a
Klaiz <109681813+Klaiz0@users.noreply.github.com>
2022-08-09 02:48:21 +0200

    Add files via upload

---
+++ klaiz_carhud/fxmanifest.lua
@@ -0,0 +1,22 @@
+-- FX Info
+fx_version 'adamant'
+game 'gta5'
+
+-- Credits
+author 'Klaiz#8244'
+description 'Esto a sido un Car-Hud modificado de el carhud pd_carhud.'
+
+-- Resources
+ui_page 'html/index.html'
+
+files {
+    "html/index.html",
+    "html/styles.css",
+    "html/digital.ttf",
+    "html/main.js"
+}
+
+client_scripts {
+    "config.lua",
+    "code/main.lua"
+}
